6ec1d4ba82283f8590d307b608acace644cacb68
Joseph Berthiaume <joenyc23@gmail.com>
2016-05-16 23:47:51 -0400

    working on framework

--- js/app.js
+++ js/app.js
@@ -1,1 +1,35 @@
+(function(){
 'use strict'
+
+var contact = {
+
+	firstName: '',
+	lastName: '',
+	telephone: '',
+	address: '',
+	city: '',
+	state: '',
+};	// end of object
+
+var ContactManager = {
+
+	createContact: function() {
+		var contact = {firstName: 'Homer', lastName: 'Simspon'}
+
+		console.log(contact.firstName);
+
+	},
+
+	store: function(contact) {
+		// shove into local storage
+	}
+};
+
+$('#btnAddContact').click(function() {
+	ContactManager.createContact();
+
+	
+});
+
+
+})();
